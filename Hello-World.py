7600beee12883347729be6325a494758d3c2675a
juicyfruit2 <134799673+juicyfruit2@users.noreply.github.com>
2023-07-01 14:38:46 +0200

    Create Hello-World.py

---
+++ Hello-World.py
@@ -0,0 +1,6 @@
+# name = Joshua -> Joshua
+name = input('Enter Your Name: ') # if input from user is joshua
+print(name) # -> print('Joshua')
+age = input('Enter Your age: ')
+print(age)
+print('Hello, World!')
